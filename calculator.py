c1b25856310d6d70477a5e9070cb79cde013e36f
cyberpranklol <kljajic.stefan96@gmail.com>
2024-01-24 11:36:05 +0100

    *

--- calculator.py
+++ calculator.py
@@ -41,10 +41,9 @@ def calculator():
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
         if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
-            num1 = answer
+            num1 = answer 
         else:
             should_continue = False
-            clear()
             calculator()
 
 
